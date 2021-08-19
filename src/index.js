11749789e5d72c1fb7b8046d45f84692048028b3
elruslan <eloyan666@gmail.com>
2021-08-19 23:41:33 +0300

    Complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = n + '';
+    let str = n.split('').reverse().join('');
+    let Number = parseInt(str, 10);
+    console.log("reversed number: ", Number);
+    return Number;
 }
+
